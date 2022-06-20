33c23937660095d048164a2c7118537c0063f8e9
nunhes <nunhes@gmail.com>
2022-06-20 23:32:17 +0200

    Update passwords.php
    Translations Update

--- locales/gl/passwords.php
+++ locales/gl/passwords.php
@@ -14,7 +14,7 @@
 return [
     'reset'     => 'O teu contrasinal foi restablecido!',
     'sent'      => 'Enviámosche por correo electrónico o enlace para restablecer o teu contrasinal!',
-    'throttled' => 'Por favor, agarde antes de repetir.',
+    'throttled' => 'Por favor, agarda antes de repetir.',
     'token'     => 'Este token de restablecemento do contrasinal non é válido.',
-    'user'      => 'Non podemos encontrar un usuario con esa dirección de correo electrónico.',
+    'user'      => 'Non podemos atopar un usuario con esa dirección de correo electrónico.',
 ];
